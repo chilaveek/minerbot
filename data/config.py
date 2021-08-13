71c86c00035650a344f66d0dd4b8dbdbf3e567f6
Nikita <nikivanov.037@gmail.com>
2021-08-13 15:31:23 +0300

    config

--- data/config.py
+++ data/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = '1895560354:AAHDyNEq-mnPe0veW-cOzRxXKXjew0VhnHI'
-PGUSER = 'postgres'
+PGUSER = 'admin'
 PGPASS = 'Q7m9kAcE2'
 ip = 'localhost'
 admins = [
