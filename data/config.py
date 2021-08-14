c4cd273874c75804e39895030d4e6b0427e398be
Nikita <nikivanov.037@gmail.com>
2021-08-14 10:25:51 +0300

    fix bugs, new command 'reset_courses'

--- data/config.py
+++ data/config.py
@@ -4,10 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = '1895560354:AAHDyNEq-mnPe0veW-cOzRxXKXjew0VhnHI'
-PGUSER = 'admin'
-PGPASS = 'Q7m9kAcE2'
-ip = 'localhost'
+BOT_TOKEN = ''
+PGUSER = ''
+PGPASS = ''
+ip = ''
 admins = [
 ]
 
