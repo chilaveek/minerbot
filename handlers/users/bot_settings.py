a35ca7ce556c88011b96475761196b1ce006e2b4
Nikita <nikivanov.037@gmail.com>
2021-08-17 12:19:07 +0300

    settings in bot

--- handlers/users/bot_settings.py
+++ handlers/users/bot_settings.py
@@ -42,6 +42,36 @@ def notifications(minerid):
     keyboard.add(*buttons)
     return keyboard
 
+def notifications_fixed(minerid):
+    miner = Miner.get(minerid=minerid)
+
+    notify_balance = 'О бунтах и забастовках'
+    notify_courses = 'О смене курса'
+    notify_reset = 'О перезапуске бота'
+
+    if miner.notify_balance is True:
+        notify_balance += '✅'
+    elif miner.notify_balance is False:
+        notify_balance += '❌'
+    if miner.notify_courses is True:
+        notify_courses += '✅'
+    elif miner.notify_courses is False:
+        notify_courses += '❌'
+    if miner.notify_reset is True:
+        notify_reset += '✅'
+    elif miner.notify_reset is False:
+        notify_reset += '❌'
+
+    buttons = [
+        InlineKeyboardButton(text=notify_balance, callback_data='balance_end_notify'),
+        InlineKeyboardButton(text=notify_courses, callback_data='change_courses_notify'),
+        InlineKeyboardButton(text=notify_reset, callback_data='reset_bot_notify'),
+    ]
+    keyboard = InlineKeyboardMarkup(row_width=1)
+    keyboard.add(*buttons)
+    return keyboard
+
+
 @dp.message_handler(text='⚙️Настройки')
 async def settings(message: types.Message):
     await message.answer(text='⚙️Открываем настройки...', reply_markup=settings_kb())
@@ -61,7 +91,7 @@ async def notify(call: CallbackQuery):
         miner.notify_balance = False
         miner.save()
     return call.message.edit_text(text='Изменения баланса приняты. Нажмите на кнопку, чтобы настроить уведомления',
-                           reply_markup=notifications(miner.minerid))
+                           reply_markup=notifications_fixed(miner.minerid))
 
 
 @dp.callback_query_handler(text='change_courses_notify')
@@ -74,7 +104,7 @@ async def notify(call: CallbackQuery):
         miner.notify_courses = False
         miner.save()
     return call.message.edit_text(text='Изменения курса приняты. Нажмите на кнопку, чтобы настроить уведомления',
-                           reply_markup=notifications(miner.minerid))
+                           reply_markup=notifications_fixed(miner.minerid))
 
 
 @dp.callback_query_handler(text='reset_bot_notify')
@@ -87,7 +117,7 @@ async def notify(call: CallbackQuery):
         miner.notify_reset = False
         miner.save()
     return call.message.edit_text(text='Изменения перезапуска приняты. Нажмите на кнопку, чтобы настроить уведомления',
-                           reply_markup=notifications(miner.minerid))
+                           reply_markup=notifications_fixed(miner.minerid))
 
 
 
